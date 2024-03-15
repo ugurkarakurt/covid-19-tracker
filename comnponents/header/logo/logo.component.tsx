6dd632c428656be3391e6172f71e1a5af58d80f5
ugurkarakurt <ugurkarakurt57@gmail.com>
2024-03-15 10:45:24 +0300

    Feat - #3 | Header > Logo & Navbar action added.

--- comnponents/header/logo/logo.component.tsx
+++ comnponents/header/logo/logo.component.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from "./logo.module.scss";
+import { Syncopate } from "next/font/google";
 import Link from 'next/link';
+import Image from 'next/image';
+import LogoImage from "@/public/logo.svg";
+
+const syncopate = Syncopate({ subsets: ["latin"], weight: '400' });
 
 function Logo() {
   return (
     <div className={styles.logoContainer}>
-      <Link href={'/'}>Covid-19 <br />Tracker</Link>
+      <Link href={'/'}>
+        <Image
+          className={styles.logo}
+          src={LogoImage}
+          unoptimized
+          alt='site_logo'
+        />
+        <h1 className={syncopate.className}>Covid-19 Tracker</h1>
+      </Link>
     </div>
   )
 }
