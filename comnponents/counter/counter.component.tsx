d43ea10e54cd9974a477ce535dc7914ef9eda5bc
ugurkarakurt <ugurkarakurt57@gmail.com>
2024-03-16 12:07:06 +0300

    Feat - #7 | Home Page > Container & World map added.

---
+++ comnponents/counter/counter.component.tsx
@@ -0,0 +1,38 @@
+import { animate, AnimationControls } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+
+interface CounterProps {
+  from: number;
+  to: number;
+}
+
+function Counter({ from, to }: CounterProps) {
+  const nodeRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    const node = nodeRef.current;
+
+    let count: AnimationControls | undefined;
+
+    if (node) {
+      count = animate(from, to, {
+        duration: 1,
+        onUpdate(value) {
+          if (node) {
+            node.textContent = value.toFixed(2);
+          }
+        }
+      });
+    }
+
+    return () => {
+      if (count) {
+        count.stop();
+      }
+    };
+  }, [from, to]);
+
+  return <span ref={nodeRef}>-</span>;
+}
+
+export default Counter;
--- containers/home/home-container.tsx
+++ containers/home/home-container.tsx
@@ -1,10 +1,41 @@
+"use client"
 import React from 'react';
 import styles from "./home-container.module.scss";
-import { useDispatch } from 'react-redux';
+import Image from 'next/image';
+import EarthImage from "@/public/earth_white.svg";
+import Counter from '@/comnponents/counter/counter.component';
 
 function HomeContainer() {
   return (
-    <div>home-container</div>
+    <div className={styles.homeContainer}>
+      <div className={styles.homeTitle}>
+        <h2>Global Stats</h2>
+        <span>Last Updated: 05.5.1203</span>
+      </div>
+      <Image
+        className={styles.earthImage}
+        src={EarthImage}
+        unoptimized
+        priority
+        alt='site_logo'
+      />
+      <div className={styles.stats}>
+        <ul className={styles.statsItems}>
+          <li className={styles.statsItem}>
+            <span className={styles.statsName}>Cases</span>
+            <Counter from={0} to={100} />
+          </li>
+          <li className={styles.statsItem}>
+            <span className={styles.statsName}>Deaths</span>
+            <Counter from={0} to={100} />
+          </li>
+          <li className={styles.statsItem}>
+            <span className={styles.statsName}>Recovered</span>
+            <Counter from={0} to={100} />
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
